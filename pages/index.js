9338442a5ebacd759eb5f5b093eedf31eb5772e7
JS junior <arnabgogoi83@gmail.com>
2023-07-20 22:39:09 +0530

    added ads

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Head from "next/head"
 import { Inter } from 'next/font/google'
 import Nav from "../components/nav"
 import Specs from "../components/desc"
@@ -14,6 +15,10 @@ export default function Home() {
   return (
     <>
     <Nav />
+    <Head>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1031665074776630"
+     crossorigin="anonymous"></script>
+    </Head>
     <main className="flex flex-col justify-center items-center my-10">
     <Specs />
     <SkillsMeter />
