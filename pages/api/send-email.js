964a410f4414a169c438fc0fdaba94370161d4ba
JS junior <arnabgogoi83@gmail.com>
2023-08-16 00:55:57 +0530

    updated email service

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -11,10 +11,11 @@ export default async function handler(req, res) {
   const { recipient, subject, htmlFilePath } = req.body;
 
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.elasticemail.com',
+    port: 2525,
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
+      pass: process.env.EMAIL_PASSKEY
     }
   });
 
